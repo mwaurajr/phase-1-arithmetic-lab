9b6e9425cf9a30404563cadbf59ea660399b5850
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-08-17 16:59:26 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+let random = Math.floor(Math.random()+1);
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+const max = Math.max(4,5,20);
